1039dd4af4e210dfb75819beae3d6310ef74192d
Donovan <donovan@tulsa.node>
2026-06-05 12:39:24 +0000

    RECTIFY_COMPILER: JVM_TARGET_ISOMORPHISM_JDK17

--- android_ui/app/build.gradle.kts
+++ android_ui/app/build.gradle.kts
@@ -28,10 +28,18 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    
+    // RECTIFIED: ELEVATED TO JDK 17 FOR PARITY
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    
+    // RECTIFIED: LOCKED KOTLIN TO JDK 17
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    
     buildFeatures {
         compose = true
     }
